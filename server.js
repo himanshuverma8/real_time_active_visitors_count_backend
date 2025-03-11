4f6b25e6e442c1ba59ee29da43e055aef58914d6
Himanshu Verma <hvinprimary@gmail.com>
2025-03-11 19:05:05 +0530

    use map to fix count issue of user having same ip with multiple devices

--- server.js
+++ server.js
@@ -14,23 +14,32 @@ const io = new Server(server, {
     },
 });
 
-const activeUsers = new Set();
-
+const activeUsers = new Map();
+//fix issue: when same ip user opens the website in two diffrent devices and closes in of them the active visitors count becomes 0 bit it needs to zero as the same ip user has open the website on his other device with same ip
 io.on("connection", (socket) => {
     let userIP = socket.handshake.headers["x-forwarded-for"] || socket.handshake.address;
     if (typeof userIP === "string") {
         userIP = userIP.split(",")[0].trim();
     }
 
-    if (!activeUsers.has(userIP)) {
-        activeUsers.add(userIP);
+    if (activeUsers.has(userIP)) {
+        activeUsers.set(userIP, activeUsers.get(userIP) + 1);
+    } else {
+        activeUsers.set(userIP, 1);
         io.emit("new-user", userIP);
     }
 
     io.emit("active-users", activeUsers.size);
 
     socket.on("disconnect", () => {
-        activeUsers.delete(userIP);
+        if (activeUsers.has(userIP)) {
+            let count = activeUsers.get(userIP);
+            if (count === 1) {
+                activeUsers.delete(userIP);
+            } else {
+                activeUsers.set(userIP, count - 1);
+            }
+        }
         io.emit("active-users", activeUsers.size);
     });
 });
